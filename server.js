f49c84ffeb496ea86d53c8c047ac5de33aacc93d
daraHK-test <newagedara@gmail.com>
2020-08-16 20:35:16 +0430

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express()
+
+// Midlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// GET - Retrieve data from the server
+app.get('/', (req, res) => {
+    res.json("Hello amazon clone");
+});
+
+// POST - Send data from frontend to backend
+app.post('/', (req, res) => {
+    console.log(req.body.name);
+    res.json("Hello amazon clone POST");
+});
+
+
+app.listen(3000, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Listening on PORT", 3000)
+    }
+})
